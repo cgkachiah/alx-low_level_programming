6efb3d6231023935cf03a95b80fa0d403c964064
cgkachiah <cgkachiah@gmail>
2021-10-11 16:50:02 +0000

    singlyList commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *nxt;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		nxt = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = nxt;
+	}
+}
